87c5d320ac68f690af9906f394d2d07b8dee23cc
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-02-27 21:57:40 +0400

    update

--- script.js
+++ script.js
@@ -5,15 +5,27 @@ const currencyFirstElement = document.getElementById("currency-first");
 const worthFirstElement = document.getElementById("worth-first");
 const currencySecondElement = document.getElementById("currency-second");
 const worthSecondElement = document.getElementById("worth-second");
-const exchangeRate = document.getElementById("exchange-rate");
+const exchangeRateElement = document.getElementById("exchange-rate");
+
+updateRate();
 
 function updateRate(e) {
-  console.log(typeof e.target.value);
   fetch(
-    `https://v6.exchangerate-api.com/v6/95142248ef1e4edb65f7b2ce/latest/${e.target.value}`
+    `https://v6.exchangerate-api.com/v6/95142248ef1e4edb65f7b2ce/latest/${currencyFirstElement.value}`
   )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      const rate = data.conversion_rates[currencySecondElement.value];
+      console.log(data);
+      exchangeRateElement.innerText = `1 ${currencyFirstElement.value} = ${
+        rate.toFixed(2) + " " + currencySecondElement.value
+      }`;
+
+      worthSecondElement.value = (
+        data.conversion_rates[currencySecondElement.value] *
+        worthFirstElement.value
+      ).toFixed(2);
+    });
 }
 
 currencyFirstElement.addEventListener("change", updateRate);
